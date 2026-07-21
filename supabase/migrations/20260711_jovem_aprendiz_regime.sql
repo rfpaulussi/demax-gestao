2171533e6270384dc583d6318f55563902f6addd
Rodolfo Paulussi <rodolfopaulussi@gmail.com>
2026-07-21 17:33:21 -0300

    fix(turnos): adiciona DROP CONSTRAINT IF EXISTS para dia_curso_range (idempotencia)

--- supabase/migrations/20260711_jovem_aprendiz_regime.sql
+++ supabase/migrations/20260711_jovem_aprendiz_regime.sql
@@ -2,6 +2,7 @@
 ALTER TABLE turnos_postos ALTER COLUMN posto_id DROP NOT NULL;
 
 ALTER TABLE horarios_funcionarios ADD COLUMN IF NOT EXISTS dia_curso SMALLINT;
+ALTER TABLE horarios_funcionarios DROP CONSTRAINT IF EXISTS dia_curso_range;
 ALTER TABLE horarios_funcionarios ADD CONSTRAINT dia_curso_range CHECK (dia_curso IS NULL OR dia_curso BETWEEN 1 AND 5);
 
 INSERT INTO turnos_postos (posto_id, nome, tipo_escala, hora_entrada, hora_saida_seg_qui, hora_saida_sex, hora_inicio_almoco, hora_fim_almoco, ativo)
